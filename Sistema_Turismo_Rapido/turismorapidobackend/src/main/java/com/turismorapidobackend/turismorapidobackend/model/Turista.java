80039a93738231ddb0eccc9aa1c4427843ab3593
Douglas Rodrigues <doug104397@gmail.com>
2023-03-14 20:48:38 -0300

    -Criação entidade Turista

---
+++ Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/model/Turista.java
@@ -0,0 +1,33 @@
+package com.turismorapidobackend.turismorapidobackend.model;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Turista {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id_turista;
+    String name;
+    long cpf;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    LocalDate data_nascimento;
+    Long tel_number;
+
+    public Turista(){
+
+    }
+
+    
+}
