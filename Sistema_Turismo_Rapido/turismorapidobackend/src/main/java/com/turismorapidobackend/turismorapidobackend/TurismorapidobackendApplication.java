baa4df5c2d34822ffaab38c35a6aa21a27fc0281
Douglas Rodrigues <doug104397@gmail.com>
2023-03-14 20:36:58 -0300

    -Upando pasta projeto Back End Spring Boot

---
+++ Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/TurismorapidobackendApplication.java
@@ -0,0 +1,13 @@
+package com.turismorapidobackend.turismorapidobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismorapidobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismorapidobackendApplication.class, args);
+	}
+
+}
---
+++ Sistema_Turismo_Rapido/turismorapidobackend/src/test/java/com/turismorapidobackend/turismorapidobackend/TurismorapidobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turismorapidobackend.turismorapidobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismorapidobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
