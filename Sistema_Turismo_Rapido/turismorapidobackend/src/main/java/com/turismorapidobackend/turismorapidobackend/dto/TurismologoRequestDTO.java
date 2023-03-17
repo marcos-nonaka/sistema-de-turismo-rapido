20807481c6823e86765580d43099b03a51060abd
Douglas Rodrigues <doug104397@gmail.com>
2023-03-17 18:46:57 -0300

    -Implementação dos atributos no TurismologoRequest

--- Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/dto/TurismologoRequestDTO.java
+++ Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/dto/TurismologoRequestDTO.java
@@ -1,5 +1,7 @@
 package com.turismorapidobackend.turismorapidobackend.dto;
 
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +11,14 @@ public class TurismologoRequestDTO {
     Long id_turismologo;
     String something;
     String name;
+    String cpf;
+    LocalDate data_nascimento;
+    Long tel_number;
+    String mail;
+    String username;
+    String password;
+
+    public TurismologoRequestDTO(){
+        
+    }
 }
