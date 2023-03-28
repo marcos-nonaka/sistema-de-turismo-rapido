c32b653d35438bbf6ed6c2eebb96617225ae16d1
Douglas Rodrigues <doug104397@gmail.com>
2023-03-28 19:24:11 -0300

    -Adicionado mais regras de acesso

--- Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/config/security/SecurityConfiguration.java
+++ Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/config/security/SecurityConfiguration.java
@@ -16,8 +16,23 @@ public class SecurityConfiguration {
         http.httpBasic();
         http
         .authorizeHttpRequests()
-        .requestMatchers(HttpMethod.GET, "/clients").permitAll()
-        .requestMatchers(HttpMethod.POST, "/clients").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.GET, "/clients").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.GET, "/roteiros").permitAll()
+        .requestMatchers(HttpMethod.GET, "/cidades").permitAll()
+        .requestMatchers(HttpMethod.GET, "/hoteis").permitAll()
+        .requestMatchers(HttpMethod.GET, "/atracoes").permitAll()
+        .requestMatchers(HttpMethod.GET, "/alimentacao").permitAll()
+        .requestMatchers(HttpMethod.POST, "/roteiros").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.POST, "/cidade").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.POST, "/hoteis").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.POST, "/atracoes").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.DELETE, "/alimentacao").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.DELETE, "/roteiros").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.DELETE, "/cidade").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.DELETE, "/hoteis").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.DELETE, "/atracoes").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.DELETE, "/alimentacao").hasRole("TURISMOLOGO")
+        .requestMatchers(HttpMethod.POST, "/clients").hasRole("ADMIN")
         .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
         .anyRequest().authenticated();
         http.csrf().disable();
