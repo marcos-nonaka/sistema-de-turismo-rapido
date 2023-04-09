da06dfbfa15ff84276b92eb434eddf44999bb3a0
Douglas Rodrigues <doug104397@gmail.com>
2023-04-09 14:03:57 -0300

    -Retorno com dados dos clientes

--- Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/dto/ClientResponseDTO.java
+++ Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/dto/ClientResponseDTO.java
@@ -1,5 +1,7 @@
 package com.turismorapidobackend.turismorapidobackend.dto;
 
+import java.time.LocalDate;
+
 import com.turismorapidobackend.turismorapidobackend.model.Client;
 
 import lombok.AllArgsConstructor;
@@ -11,6 +13,11 @@ public class ClientResponseDTO {
     String name;
     Long idClient;
     String photo;
+    String cpf;
+    LocalDate data_nascimento;
+    Long tel_number;
+    String mail;
+    String username;
     // Long id_turista;
     // Long id_turismologo;
 
@@ -18,6 +25,11 @@ public class ClientResponseDTO {
         this.name = client.getName();
         this.idClient = client.getIdClient();
         this.photo = client.getPhoto();
+        this.cpf = client.getCpf();
+        this.data_nascimento = client.getData_nascimento();
+        this.tel_number = client.getTel_number();
+        this.mail = client.getMail();
+        this.username = client.getUsername();
         //usuario e senha
         //nome
         //email
