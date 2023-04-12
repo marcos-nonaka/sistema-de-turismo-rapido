482fb7f16015248e7082196891636fdf8c810f01
Robson Silva <robsonveloso@hotmail.com>
2023-04-12 18:47:49 -0300

    Inclusão atributo photo na Entidade Cidade.

--- Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/dto/CidadeResponseDTO.java
+++ Sistema_Turismo_Rapido/turismorapidobackend/src/main/java/com/turismorapidobackend/turismorapidobackend/dto/CidadeResponseDTO.java
@@ -15,6 +15,7 @@ public class CidadeResponseDTO {
     List<IdNameDTO> atracoes;
     List<IdNameDTO> alimentacoes;
     List<IdNameDTO> hotels;
+    String photo;
 
     public CidadeResponseDTO() {
     }
@@ -22,6 +23,7 @@ public class CidadeResponseDTO {
     public CidadeResponseDTO(Cidade cidade) {
         this.idCidade = cidade.getIdCidade();
         this.name = cidade.getName();
+        this.photo = cidade.getPhoto();
         this.atracoes = cidade.getAtracoes().stream().map(atracao -> new IdNameDTO(atracao.getIdAtracao(), atracao.getName())).toList();
         this.alimentacoes = cidade.getAlimentacoes().stream().map(alimentacao -> new IdNameDTO(alimentacao.getId_alimentacao(), alimentacao.getLocal_name())).toList();
         this.hotels = cidade.getHotels().stream().map(hotel -> new IdNameDTO(hotel.getIdHotel(), hotel.getHotelName()) ).toList();
